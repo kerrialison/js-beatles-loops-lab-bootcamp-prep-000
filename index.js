156280dae451a3b7e5f7dd0f9af4fdd1eb21e60f
Kerri Varner <kerrivarner@gmail.com>
2018-11-19 18:12:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for (let i = 0, l = musicians.length; i < l; i++ ){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array;
+}
+
+function johnLennonFacts(facts){
+  const johnFacts = []
+
+  let i = 0
+  while(i < facts.length){
+  johnFacts.push(`${facts[i]}!!!`)
+  i++
+  }
+
+  return johnFacts;
+  }
